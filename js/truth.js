f1bf82901312952d168908ca4cadedfb6e132d20
yl-2010 <yl-2010@outlook.com>
2024-10-21 12:52:59 -0700

    ic

--- js/truth.js
+++ js/truth.js
@@ -4,4 +4,9 @@ function greet() {
     let greetParagraph = document.getElementById("greet");
 
     greetParagraph.style.color = "green"
+
+    let lname = document.getElementById("lname").value;
+    let age = document.getElementById("age").value;
+
+    
 }
