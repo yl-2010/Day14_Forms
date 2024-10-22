abf54697b30a7e68b9943843299711e0bf30eaa6
yl-2010 <yl-2010@outlook.com>
2024-10-22 14:30:20 -0700

    Finished first question.

--- js/truth.js
+++ js/truth.js
@@ -5,7 +5,7 @@ function greet() {
 
     greetParagraph.style.color = "green"
 
-    let fname = document.getElementById("fname").value;
+    fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let age = document.getElementById("age").value;
 
@@ -15,7 +15,6 @@ function greet() {
 function trivia1() {
     let triviaAnswer = document.getElementById("trivia1-answer");
 
-    let fname = document.getElementById("fname").value;
 
     // get values from form
     let chocolateSelected = document.getElementById("chocolate").checked;
@@ -36,23 +35,25 @@ function trivia1() {
 
 
 function q1() {
-    let triviaAnswer = document.getElementById("trivia1-answer");
+    let q1Answer = document.getElementById("q1-answer");
 
-    let fname = document.getElementById("fname").value;
 
     // get values from form
-    let chocolateSelected = document.getElementById("chocolate").checked;
-    let tunaSelected = document.getElementById("tuna").checked;
-    let honeySelected = document.getElementById("honey").checked;
+    let htmlSelected = document.getElementById("r1").checked;
+    let roboticsSelected = document.getElementById("r2").checked;
+    let chromeSelected = document.getElementById("r3").checked;
 
     // give result based off of what they selected
-    if (chocolateSelected) {
-        triviaAnswer.innerHTML = fname + ", you are wrong. Sweet tooth eh?";
+    if (htmlSelected) {
+        q1Answer.innerHTML = fname + ", you are wrong. This <em>is</em> my first time learning HTML.";
     }
-    else if (tunaSelected) {
-        triviaAnswer.innerHTML = fname + ", you are wrong. Rotten tuna, yuck!"
+    else if (roboticsSelected) {
+        q1Answer.innerHTML = fname + ", you are right! I did robotics for <em>two</em> years in middle school."
     }
-    else if (honeySelected) {
-        triviaAnswer.innerHTML = fname + ", you are correct!";
+    else if (chromeSelected) {
+        q1Answer.innerHTML = fname + ", you are wrong. I use the <a href='https://www.arc.net' target='_blank'>Arc Browser</a>.";
+    }
+    else {
+        q1Answer.innerHTML = fname + ", select something!";
     }
 }
